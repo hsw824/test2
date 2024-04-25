b47ebd01a72b3d41f6e2f81b73efaed0560c37d4
hsw824 <ghdtjddn0824@naver.com>
2024-04-26 00:12:48 +0900

    feat: 어드민 페이지 기본 셋팅

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import styles from "./Admin.module.scss";
+import AdminItem from "./AdminItem";
 export default function Admin() {
-  return <div>어드민 페이지</div>;
+  const [user, setUser] = useState([
+    {
+      id: 0,
+      userName: "김민수",
+      grade: "새싹1",
+      age: "21",
+      gender: "남자",
+    },
+    {
+      id: 1,
+      userName: "홍길동",
+      grade: "새싹2",
+      age: "21",
+      gender: "여자",
+    },
+  ]);
+  return (
+    <div className={styles.container}>
+      {user.map((info) => {
+        return <AdminItem userInfo={info} key={info.id} />;
+      })}
+
+      <button className={styles.button}>+</button>
+    </div>
+  );
 }
